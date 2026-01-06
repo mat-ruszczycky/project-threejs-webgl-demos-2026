ee0ce74daf81823bfa9c4515336fe0897b765d69
Mat Ruszczycky (V) <mat_ruszczycky@apple.com>
2026-01-06 09:21:56 -0800

    chore: checkpoint

--- src/scripts/core-002/objects.js
+++ src/scripts/core-002/objects.js
@@ -6,20 +6,16 @@ export function createLights(scene) {
 }
 
 export function createBallMesh(radius = 0.5) {
-  const mesh = new THREE.Mesh(
+  return new THREE.Mesh(
     new THREE.SphereGeometry(radius, 10, 10),
     new THREE.MeshStandardMaterial({ roughness: 0.7, wireframe: true })
   );
-
-  return mesh;
 }
 
 export function createBoxMesh(dims = { w: 9, h: 1.5, d: 9 }) {
   const { w, h, d } = dims;
-  const mesh = new THREE.Mesh(
+  return new THREE.Mesh(
     new THREE.BoxGeometry(w, h, d),
     new THREE.MeshStandardMaterial({ roughness: 0.7, wireframe: false })
   );
-
-  return mesh;
 }

6854bafcb54295d0d2d422ac4b4cabe7aa11ff06
Mat Ruszczycky (V) <mat_ruszczycky@apple.com>
2026-01-06 19:57:17 -0800

    chore: checkpoint

--- src/scripts/core-002/index.js
+++ src/scripts/core-002/index.js
@@ -1,3 +1,6 @@
-import * as CORE from "./exports";
+// Re-export selected modules for named imports
 export * from "./exports";
+
+// Provide a default export that bundles all modules for single-import usage
+import * as CORE from "./exports";
 export default CORE;
